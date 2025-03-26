a6b38ee38bcb2de4f6ead611897c657a7f8cfbcb
Carter Stevens <carter@forthwithlife.co.uk>
2025-03-26 15:05:10 +0000

    create jobs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,6 +9,52 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '£50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '£45,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '£40,000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{jobID}', function ($jobID) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '£50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '£45,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '£40,000'
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $jobID);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
