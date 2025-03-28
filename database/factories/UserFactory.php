bd2a7d574a3677ae86ac27b20af62e761e2e215d
Carter Stevens <carter@forthwithlife.co.uk>
2025-03-28 11:55:26 +0000

    add employer with id and factory aswell as job factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,15 +29,30 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'admin' => false,
         ];
+    }
+
+    /**
+     * Indicate that the model's admin should be true.
+     */
+    // php artisan tinker
+    // App\Models\User::factory()->admin()->create(); 
+    public function admin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'admin' => true,
+        ]);
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
+    // php artisan tinker
+    // App\Models\User::factory()->unverified()->create(); 
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
