024fe9621bc823d3a02bcf226276bfac25e483ae
Carter Stevens <carter@forthwithlife.co.uk>
2025-03-28 09:38:30 +0000

    Move jobs to a class space

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -13,46 +14,12 @@ Route::get('/about', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '£50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '£45,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '£40,000'
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{jobID}', function ($jobID) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '£50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '£45,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '£40,000'
-        ],
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $jobID);
+Route::get('/jobs/{jobID}', function ($id) {
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
